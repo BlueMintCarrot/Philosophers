54123aa926d83d0d6269ca028dcad845ed3df95b
BlueMintCarrot <joana.tavares.55@gmail.com>
2024-05-15 01:00:14 +0100

    update

--- main.c
+++ main.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joanda-s <joanda-s@student.42.fr>          +#+  +:+       +#+        */
+/*   By: joana <joana@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 16:01:46 by joanda-s          #+#    #+#             */
-/*   Updated: 2024/05/03 20:59:44 by joanda-s         ###   ########.fr       */
+/*   Updated: 2024/05/15 00:59:58 by joana            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+#define M "How would you run a simulation without any philosophers??\n"
 
 long long	ft_llato(const char *str)
 {
@@ -34,10 +35,12 @@ long long	ft_llato(const char *str)
 		z = (z * 10) + (str[x] - 48);
 		x++;
 	}
+	if ((str[x] < 48 || str[x] > 57) && str[x] != '\0')
+		return (0);
 	return (z * y);
 }
 
-t_info	*info_init(int argc, char **argv)
+t_info	*info_init(char **argv)
 {
 	int				n;
 	t_info			*info;
@@ -49,7 +52,8 @@ t_info	*info_init(int argc, char **argv)
 	info->time_til_death = ft_llato(argv[2]);
 	info->time_to_eat = ft_llato(argv[3]);
 	info->time_to_sleep = ft_llato(argv[4]);
-	info->nbr_of_meals = ft_llato(argv[5]);
+	info->nbr_of_full_philo = 0;
+	info->still_alive = 1;
 	pthread_mutex_init(&info->lock, NULL);
 	pthread_mutex_init(&info->beep, NULL);
 	gettimeofday(&time, NULL);
@@ -60,33 +64,160 @@ t_info	*info_init(int argc, char **argv)
 		pthread_mutex_init(&info->fork[n], NULL);
 		n++;
 	}
+	return (info);
 }
 
-t_philo	*philo_init(t_info *info)
+int	input_verifications(t_philo *philo, int argc)
+{
+	if (philo->info->nbr_of_philo < 1)
+	{
+		write(1, M, 58);
+		return (0);
+	}
+	if (philo->info->time_til_death < 1 || philo->info->time_to_eat < 1
+		|| philo->info->time_to_sleep < 1)
+	{
+		write(1, "Invalid times to perform actions!\n", 34);
+		return (0);
+	}
+	if (philo->nbr_of_meals <= 0 && argc == 6)
+	{
+		write(1, "No meals available, they will starve!\n", 38);
+		return (0);
+	}
+	return (1);
+}
+
+void	are_you_still_there_helper(t_philo *philo, int n)
+{
+	long int	sacred_time;
+
+	pthread_mutex_lock(&philo->info->lock);
+	philo->info->still_alive = 0;
+	pthread_mutex_unlock(&philo->info->lock);
+	pthread_mutex_lock(&philo->info->beep);
+	sacred_time = tva() - philo->info->start_time;
+	ft_putnbr(sacred_time);
+	write(1, " ", 1);
+	ft_putnbr(n + 1);
+	write(1, " died\n", 6);
+	pthread_mutex_unlock(&philo->info->beep);
+}
+
+void	are_you_still_there(t_philo *philo)
+{
+	int	n;
+
+	n = 0;
+	while (1)
+	{
+		pthread_mutex_lock(&philo[n].chronos);
+		if (tva() > philo[n].time_til_death && philo[n].nbr_of_meals != 0)
+		{
+			pthread_mutex_unlock(&philo[n].chronos);
+			break ;
+		}
+		pthread_mutex_unlock(&philo[n].chronos);
+		if (philo->info->nbr_of_full_philo == philo->info->nbr_of_philo)
+		{
+			pthread_mutex_lock(&philo->info->lock);
+			philo->info->still_alive = 0;
+			pthread_mutex_unlock(&philo->info->lock);
+			return ;
+		}
+		n++;
+		if (n == philo->info->nbr_of_philo)
+			n = 0;
+	}
+	are_you_still_there_helper(philo, n);
+}
+
+void	athena(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->info->lock);
+	if (philo->info->still_alive == 0)
+	{
+		pthread_mutex_unlock(&philo->info->lock);
+		return ;
+	}
+	pthread_mutex_unlock(&philo->info->lock);
+	leave_a_message_after_the_beep(philo, 4);
+	if (philo->info->nbr_of_philo % 2 == 0)
+		philo->info->time_to_think = philo->info->time_to_eat
+			- philo->info->time_to_sleep;
+	else if (philo->info->nbr_of_philo % 2 == 1)
+		philo->info->time_to_think = ((philo->info->time_to_eat * 2)
+				- philo->info->time_to_sleep) * 0.42;
+	hypnos(tva() + philo->info->time_to_think);
+	eat_sleep_repeat(philo);
+}
+
+void	morpheus(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->info->lock);
+	if (philo->info->still_alive == 0)
+	{
+		pthread_mutex_unlock(&philo->info->lock);
+		return ;
+	}
+	pthread_mutex_unlock(&philo->info->lock);
+	leave_a_message_after_the_beep(philo, 3);
+	hypnos(tva() + philo->info->time_to_sleep);
+	athena(philo);
+}
+
+t_philo	*philo_init(t_info *info, char **argv)
 {
 	t_philo	*philo;
-	int	n;
+	int		n;
 
 	philo = malloc(sizeof(t_philo) * info->nbr_of_philo);
 	n = 0;
 	while (n < info->nbr_of_philo)
 	{
 		philo[n].info = info;
-		philo[n].time_til_death = info->time_til_death;
+		philo[n].time_til_death = info->start_time + info->time_til_death;
+		if (argv[5])
+			philo[n].nbr_of_meals = ft_llato(argv[5]);
+		else
+			philo[n].nbr_of_meals = -1;
 		philo[n].seat_pos = n + 1;
+		pthread_mutex_init(&philo[n].chronos, NULL);
 		n++;
 	}
+	return (philo);
 }
 
-void	*eat_sleep_repeat(void *philo)
+void	*eat_sleep_repeat(void *philos)
 {
 	t_philo	*philo;
 
-	philo = (t_philo *)philo;
+	philo = (t_philo *)philos;
+	if (philo->nbr_of_meals == 0)
+		philo->info->nbr_of_full_philo++;
+	pthread_mutex_lock(&philo->info->lock);
+	if (philo->info->still_alive == 0)
+	{
+		pthread_mutex_unlock(&philo->info->lock);
+		return (NULL);
+	}
+	pthread_mutex_unlock(&philo->info->lock);
 	pick_up_forks(philo);
-	
+	pthread_mutex_lock(&philo->chronos);
+	philo->time_til_death = tva() + philo->info->time_til_death;
+	pthread_mutex_unlock(&philo->chronos);
+	leave_a_message_after_the_beep(philo, 2);
+	philo->nbr_of_meals--;
+	hypnos(tva() + philo->info->time_to_eat);
 	put_down_forks(philo);
+	morpheus(philo);
 	return (NULL);
+}
+
+void	hypnos(long int time_limit)
+{
+	while (tva() < time_limit)
+		usleep(300);
 }
 
 void	pick_up_forks(t_philo *philo)
@@ -97,23 +228,23 @@ void	pick_up_forks(t_philo *philo)
 			pthread_mutex_lock(&philo->info->fork[0]);
 		else
 			pthread_mutex_lock(&philo->info->fork[philo->seat_pos]);
-		leave_a_message_after_the_beep(philo);
+		leave_a_message_after_the_beep(philo, 1);
 		pthread_mutex_lock(&philo->info->fork[philo->seat_pos - 1]);
-		leave_a_message_after_the_beep(philo);
+		leave_a_message_after_the_beep(philo, 1);
 	}
 	else if (philo->seat_pos % 2 == 1)
 	{
 		pthread_mutex_lock(&philo->info->fork[philo->seat_pos - 1]);
-		leave_a_message_after_the_beep(philo);
+		leave_a_message_after_the_beep(philo, 1);
 		if (philo->seat_pos == philo->info->nbr_of_philo)
 			pthread_mutex_lock(&philo->info->fork[0]);
 		else
 			pthread_mutex_lock(&philo->info->fork[philo->seat_pos]);
-		leave_a_message_after_the_beep(philo);
+		leave_a_message_after_the_beep(philo, 1);
 	}
 }
 
-long	tva(void)
+long int	tva(void)
 {
 	struct timeval	time;
 	long int		sacred_time;
@@ -123,9 +254,62 @@ long	tva(void)
 	return (sacred_time);
 }
 
-void	leave_a_message_after_the_beep(t_philo *philo)
+void	ft_putchar(char c)
 {
+	write (1, &c, 1);
+}
+
+void	ft_putnbr(long int nb)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(nb * -1);
+	}
+	else if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + '0');
+}
+
+//1 - pick up fork;
+//2 - is eating;
+//3 - is sleeping;
+//4 - is thinking;
+//5 - has falecido;
+
+void	leave_a_message_after_the_beep(t_philo *philo, int i)
+{
+	long int	sacred_time;
+
 	pthread_mutex_lock(&philo->info->beep);
+	pthread_mutex_lock(&philo->info->lock);
+	if (philo->info->still_alive == 0)
+	{
+		pthread_mutex_unlock(&philo->info->lock);
+		pthread_mutex_unlock(&philo->info->beep);
+		return ;
+	}
+	pthread_mutex_unlock(&philo->info->lock);
+	sacred_time = tva() - philo->info->start_time;
+	ft_putnbr(sacred_time);
+	write(1, " ", 1);
+	ft_putnbr(philo->seat_pos);
+	write(1, " ", 1);
+	if (i == 1)
+		write(1, "has taken a fork\n", 17);
+	else if (i == 2)
+		write(1, "is eating\n", 10);
+	else if (i == 3)
+		write (1, "is sleeping\n", 12);
+	else if (i == 4)
+		write (1, "is thinking\n", 12);
+	else if (i == 5)
+		write (1, "died\n", 5);
+	pthread_mutex_unlock(&philo->info->beep);
 }
 
 void	put_down_forks(t_philo *philo)
@@ -136,41 +320,75 @@ void	put_down_forks(t_philo *philo)
 			pthread_mutex_unlock(&philo->info->fork[0]);
 		else
 			pthread_mutex_unlock(&philo->info->fork[philo->seat_pos]);
-		leave_a_message_after_the_beep(philo);
 		pthread_mutex_unlock(&philo->info->fork[philo->seat_pos - 1]);
-		leave_a_message_after_the_beep(philo);
 	}
 	else if (philo->seat_pos % 2 == 1)
 	{
 		pthread_mutex_unlock(&philo->info->fork[philo->seat_pos - 1]);
-		leave_a_message_after_the_beep(philo);
 		if (philo->seat_pos == philo->info->nbr_of_philo)
 			pthread_mutex_unlock(&philo->info->fork[0]);
 		else
 			pthread_mutex_unlock(&philo->info->fork[philo->seat_pos]);
-		leave_a_message_after_the_beep(philo);
 	}
 }
 
-int main(int argc, char **argv)
+void	philo_cleaner(t_philo *philo)
 {
-	t_info	*info;
-	t_philo	*philo;
 	int	n;
 
 	n = 0;
-	info = info_init(argc, argv);
-	philo = philo_init(info);
+	pthread_mutex_destroy(&philo->info->beep);
+	while (n < philo->info->nbr_of_philo)
+	{
+		pthread_mutex_destroy(&philo->info->fork[n]);
+		pthread_mutex_destroy(&philo[n].chronos);
+		n++;
+	}
+	pthread_mutex_destroy(&philo->info->lock);
+	free(philo->info->fork);
+	free(philo->info);
+	free(philo);
+}
+
+int	main(int argc, char **argv)
+{
+	t_info	*info;
+	t_philo	*philo;
+	int		n;
+
+	n = 0;
+	if (argc < 5 || argc > 6)
+	{
+		if (argc < 5)
+			write(1, "Not enough arguments!\n", 22);
+		else
+			write(1, "Too many arguments!\n", 20);
+		return (0);
+	}
+	info = info_init(argv);
+	philo = philo_init(info, argv);
+	if (input_verifications(philo, argc) == 0)
+	{
+		philo_cleaner(philo);
+		return (0);
+	}
 	while (n < info->nbr_of_philo)
 	{
 		pthread_create(&philo[n].id, NULL, eat_sleep_repeat, &philo[n]);
 		n++;
 	}
+	are_you_still_there(philo);
 	n = 0;
 	while (n < info->nbr_of_philo)
 	{
 		pthread_join(philo[n].id, NULL);
 		n++;
 	}
+	philo_cleaner(philo);
 	return (0);
 }
+
+/* 1 filosofo loop infinito
+segfault com 0 ou nr de filosofos negativo
+They're dying randomly
+They're not eating properly */
--- philosophers.h
+++ philosophers.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philosophers.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joanda-s <joanda-s@student.42.fr>          +#+  +:+       +#+        */
+/*   By: joana <joana@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 15:34:56 by joanda-s          #+#    #+#             */
-/*   Updated: 2024/05/03 17:18:52 by joanda-s         ###   ########.fr       */
+/*   Updated: 2024/05/15 00:16:45 by joana            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,30 +21,46 @@
 # include <string.h>
 # include <limits.h>
 
-
-typedef struct	s_philo
+typedef struct s_info
 {
-	int			seat_pos;
-	int			still_alive;
-	long int	time_til_death;
-	pthread_t	id;
-	t_info		*info;
-}				t_philo;
-
-typedef struct	s_info
-{
-	int				nbr_of_meals;
-    int				nbr_of_philo;
-    long int		time_til_death;
+	int				nbr_of_philo;
+	int				nbr_of_full_philo;
+	int				still_alive;
 	long int		start_time;
+	long int		time_til_death;
 	long int		time_to_eat;
 	long int		time_to_sleep;
 	long int		time_to_think;
 	pthread_mutex_t	beep;
 	pthread_mutex_t	*fork;
 	pthread_mutex_t	lock;
-	
 }				t_info;
 
+typedef struct s_philo
+{
+	int			seat_pos;
+	int			still_alive;
+	int			nbr_of_meals;
+	long int	time_til_death;
+	pthread_t	id;
+	pthread_mutex_t	chronos;
+	t_info		*info;
+}				t_philo;
+
+long int	tva(void);
+long long	ft_llato(const char *str);
+t_info		*info_init(char **argv);
+t_philo		*philo_init(t_info *info, char **argv);
+void		are_you_still_there(t_philo *philo);
+void		athena(t_philo *philo);
+void		*eat_sleep_repeat(void *philos);
+void		ft_putchar(char c);
+void		ft_putnbr(long int nb);
+void		hypnos(long int time_limit);
+void		leave_a_message_after_the_beep(t_philo *philo, int i);
+void		morpheus(t_philo *philo);
+void		philo_cleaner(t_philo *philo);
+void		pick_up_forks(t_philo *philo);
+void		put_down_forks(t_philo *philo);
 
 #endif
